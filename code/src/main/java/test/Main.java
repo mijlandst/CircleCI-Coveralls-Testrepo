bce63e0dee79cd26cbab9a9ffef6830520e0be5c
Stijn Mijland <s.t.mijland@student.rug.nl>
2017-08-26 16:29:06 +0200

    Added dummy code + initial gitignore

---
+++ code/src/main/java/test/Main.java
@@ -0,0 +1,16 @@
+package test;
+
+/**
+ * A dummy program to enable the creation of elementary test to verify that
+ * the CI works.
+ */
+public class Main {
+
+    /**
+     * Prints "Hello, world!" to standard output.
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+    }
+    
+}
